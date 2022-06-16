799003071ad25642de05ff4fbe980b77134df878
https://github.com/TaiseiUgawa <taisei8808@gmai.com>
2022-06-16 13:17:49 +0900

    test2

--- Study/main.js
+++ Study/main.js
@@ -1,2 +1,21 @@
-const str = 'hello';
-console.log(str);
+// Object 
+const member = {
+    'name': '本田',
+    'height': 170,
+    'hobby': 'サッカー',
+};
+
+const member_2 = {
+    '本田':{
+        'height': 170,
+        'hobby': 'サッカー',
+    },
+    '香川':{
+        'height': 174,
+        'hobby': 'テニス',
+    },
+}
+
+console.log(member_2.本田.hobby);
+
+// 演算子
